51df1ee6978752e8ef8bf46ed45251cb3f181547
AleksandrsBaranovs <125829016+AleksandrsBaranovs@users.noreply.github.com>
2023-03-05 17:35:43 +0000

    2.uzd

--- tree_height.py
+++ tree_height.py
@@ -5,24 +5,31 @@ import threading
 import numpy
 
 def compute_height(n, parents):
+    
     tree = {i: [] for i in range(n)}
     for i in range(n):
         if parents[i] != -1:
             tree[parents[i]].append(i)
-    
+
+   
     def height(node):
         if not tree[node]:
-            return 0
+            return 1
         else:
             return 1 + max(height(child) for child in tree[node])
 
-   
-    return height(parents.index(-1))
+    
+    root = next(i for i, p in enumerate(parents) if p == -1)
+    return height(root)
 
 def main():
-    n = int(input(""))
+    
+    n = int(input())
     parents = list(map(int, input().split()))
+
+    
     print(compute_height(n, parents))
+
 
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
